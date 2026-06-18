decf1cc2fadd2e87305fc0d54f3cc26fe42aa513
Ramazanenescik04 <114609990+Ramazanenescik04@users.noreply.github.com>
2026-06-19 01:07:19 +0300

    delete all main branch codes

--- DikenEngine/src/me/ramazanenescik04/diken/gui/component/IGuiListener.java
+++
@@ -1,9 +0,0 @@
-package me.ramazanenescik04.diken.gui.component;
-
-import me.ramazanenescik04.diken.gui.hitbox.IHitbox;
-
-public interface IGuiListener {
-	void changedBounds(IHitbox newBounds);
-	void changedSize(IHitbox newBounds, int width, int height);
-	void changedLocation(IHitbox newBounds, int x, int y);
-}
--- DikenEngine/src/me/ramazanenescik04/diken/resource/EnumResource.java
+++
@@ -1,8 +0,0 @@
-package me.ramazanenescik04.diken.resource;
-
-/**
- * Enumerates the `EnumResource` type within the DikenEngine `resource` package.
- */
-public enum EnumResource {
-	IMAGE, SOUND, CURSOR, ANIMATION
-}
